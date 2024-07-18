2ab3e24ebe85f4479edfd720c5caad47324341cd
udaykumardl <udaykumardl744@gmail.com>
2024-07-18 20:15:28 +0530

    Loading the application

--- src/Components/MovieData/MovieData.js
+++ src/Components/MovieData/MovieData.js
@@ -3,8 +3,11 @@ import classes from './MovieData.module.css'
 
 const MovieData =() =>{
     const [movies ,setMovies] =useState ([])
+    const [isLoading, setIsLoading]=useState(false)
 
     async function fetchMoviesHandler() {
+        try{
+        setIsLoading(true)
         const response= await fetch('https://swapi.dev/api/films/')
          const data= await response.json()
        
@@ -14,10 +17,14 @@ const MovieData =() =>{
                     title:movieData.title,
                     openingText:movieData.opening_crawl,
                     releaseDate:movieData.release_date
-                }
-            ))
+                }))
+
             setMovies(transformedMovies);
-        
+            setIsLoading(false)
+        }
+            catch (error) {
+                console.error('Error fetching movies:', error);
+            }
         }
     
 
@@ -26,7 +33,7 @@ const MovieData =() =>{
         <button className={classes.button} onClick={fetchMoviesHandler}>Fetch Movie</button>
             
             <div className={classes.maindiv}>
-                {movies.map((movie, index) => (
+                {!isLoading && movies.length>0 &&  movies.map((movie, index) => (
                     <div className={classes.item} key={index}>
                         <div className={classes.innerdiv}>
                             <h3 className={classes.place}>{movie.title}</h3>
@@ -37,6 +44,8 @@ const MovieData =() =>{
                         <td><button>BUY TICKETS</button></td>
                     </div>
                 ))}
+                {!isLoading && movies.length===0 && <p>Found no movies</p>}
+                {isLoading && <p>Loading ...</p>}
             </div>
 
     </div>
@@ -47,53 +56,3 @@ const MovieData =() =>{
 
 export default MovieData;
 
-
-
-// import React, { useState } from 'react';
-// import classes from './MovieData.module.css';
-
-// const MovieData = () => {
-//     const [movies, setMovies] = useState([]);
-
-//     async function fetchMovieHandler() {
-//         try {
-//             const response = await fetch('https://swapi.dev/api/films/');
-//             const data = await response.json();
-
-//             const transformedMovies = data.results.map(movieData => ({
-//                 id: movieData.episode_id,
-//                 title: movieData.title,
-//                 openingText: movieData.opening_crawl,
-//                 releaseDate: movieData.release_date
-//             }));
-
-//             setMovies(transformedMovies);
-//         } catch (error) {
-//             console.error('Error fetching movies:', error);
-//         }
-//     }
-
-//     return (
-//         <div>
-//             <button className={classes.button} onClick={fetchMovieHandler}>Fetch Movie</button>
-
-
-//                 <div className={classes.maindiv}>
-//                     {movies.map((movie, index) => (
-//                         <div className={classes.item} key={index}>
-//                             <div className={classes.innerdiv}>
-//                               <h3 className={classes.place}>{movie.title}</h3>
-//                             <h5 className={classes.date}>Release Date:{movie.releaseDate}</h5>
-//                             </div>
-
-//                             <td className={classes.city}>{movie.openingText}</td><br/>
-//                             <td><button>BUY TICKETS</button></td>
-//                         </div>
-//                     ))}
-//                 </div>
-
-//         </div>
-//     );
-// }
-
-// export default MovieData;
